3fc2a56f32f1e3cd27181fb3ded4ed305d8d72a1
eduardodoria <doriaeduardo@gmail.com>
2022-12-12 14:35:44 -0300

    Added action with independent TimedAction

--- actions/lua/main.lua
+++ actions/lua/main.lua
@@ -1,17 +1,27 @@
 scene = Scene()
 
+text = Text(scene)
+
 polygon = MeshPolygon(scene)
 polygon2 = MeshPolygon(scene)
 polygon3 = Polygon(scene)
+polygon4 = Polygon(scene)
+
 positionaction = PositionAction(scene)
 rotationaction = RotationAction(scene)
 scaleaction = ScaleAction(scene)
 coloraction = ColorAction(scene)
 alphaaction = AlphaAction(scene)
+timedaction = TimedAction(scene)
 
+
+function onActionStep()
+    angle = math.pi * 2.0 * timedaction:getValue()
+    polygon4:setPosition(math.cos(angle)*100 + 450, math.sin(angle)*100 + 300, 0);
+end
 
 function onActionStart()
-    Log.verbose("Action start")
+    Log.verbose("Rotation action start")
 end
 
 function onTouchStart(pointer, x, y)
@@ -20,31 +30,46 @@ function onTouchStart(pointer, x, y)
     scaleaction:start()
     coloraction:start()
     alphaaction:start()
+    timedaction:start()
 end
 
+
+text.text = "Click on the screen to start"
+text.anchorPreset = AnchorPreset.CENTER_TOP
+
 polygon:addVertex(0, 0);
-polygon:addVertex(200, 0);
-polygon:addVertex(0, 200);
-polygon:addVertex(200, 200);
+polygon:addVertex(100, 0);
+polygon:addVertex(0, 100);
+polygon:addVertex(100, 100);
 polygon:setColor(1.0, 0.3, 0.8, 1.0);
 polygon:setPosition(50, 50, 0);
 polygon.name = "Polygon1";
 
 polygon2:addVertex(0, 0)
-polygon2:addVertex(200, 0)
-polygon2:addVertex(0, 200)
-polygon2:addVertex(200, 200)
+polygon2:addVertex(100, 0)
+polygon2:addVertex(0, 100)
+polygon2:addVertex(100, 100)
 polygon2:setColor(1.0, 0.5, 1.0, 1.0)
-polygon2:setPosition(350, 50, 0)
-polygon.name = "Polygon2"
+polygon2:setPosition(450, 50, 0)
+polygon2.name = "Polygon2"
 
 polygon3:addVertex(0, 0)
-polygon3:addVertex(200, 0)
-polygon3:addVertex(0, 200)
-polygon3:addVertex(200, 200)
+polygon3:addVertex(100, 0)
+polygon3:addVertex(0, 100)
+polygon3:addVertex(100, 100)
 polygon3:setColor(0.5, 1.0, 1.0, 1.0)
-polygon3:setPosition(650, 50, 0)
-polygon.name = "Polygon3"
+polygon3:setPosition(800, 50, 0)
+polygon3.name = "Polygon3"
+
+polygon4:addVertex(0, 0)
+polygon4:addVertex(100, 0)
+polygon4:addVertex(0, 100)
+polygon4:addVertex(100, 100)
+polygon4:setColor(0.5, 1.0, 0.3, 1.0)
+polygon4:setPosition(450, 300, 0)
+polygon4.name = "Polygon4"
+
+polygon2:moveToFirst();
 
 startRot = Quaternion()
 endRot = Quaternion()
@@ -67,6 +92,9 @@ positionaction:setAction(Vector3(20,20,0), Vector3(200,200,0), 5, true)
 positionaction.target = polygon.entity
 positionaction:setFunctionType(EaseType.ELASTIC_IN_OUT)
 positionaction.speed = 1
+
+timedaction.duration = 10
+timedaction:getActionComponent().onStep = onActionStep;
 
 Engine.setScene(scene)
 Engine.setCallTouchInMouseEvent(true)
--- actions/main.cpp
+++ actions/main.cpp
@@ -10,47 +10,66 @@ using namespace Supernova;
 #include "ScaleAction.h"
 #include "ColorAction.h"
 #include "AlphaAction.h"
+#include "Text.h"
 
 Scene scene;
+
+Text text(&scene);
 
 MeshPolygon polygon(&scene);
 MeshPolygon polygon2(&scene);
 Polygon polygon3(&scene);
+Polygon polygon4(&scene);
+
+TimedAction timedaction(&scene);
 PositionAction positionaction(&scene);
 RotationAction rotationaction(&scene);
 ScaleAction scaleaction(&scene);
 ColorAction coloraction(&scene);
 AlphaAction alphaaction(&scene);
 
-
+void onActionStep();
 void onActionStart();
 void onTouchStart(int pointer, float x, float y);
 
 void init(){
 
+    text.setText("Click on the screen to start");
+    text.setAnchorPreset(AnchorPreset::CENTER_TOP);
+
     polygon.addVertex(0, 0);
-    polygon.addVertex(200, 0);
-    polygon.addVertex(0, 200);
-    polygon.addVertex(200, 200);
+    polygon.addVertex(100, 0);
+    polygon.addVertex(0, 100);
+    polygon.addVertex(100, 100);
     polygon.setColor(1.0, 0.3, 0.8, 1.0);
     polygon.setPosition(50, 50, 0);
     polygon.setName("Polygon1");
 
     polygon2.addVertex(0, 0);
-    polygon2.addVertex(200, 0);
-    polygon2.addVertex(0, 200);
-    polygon2.addVertex(200, 200);
+    polygon2.addVertex(100, 0);
+    polygon2.addVertex(0, 100);
+    polygon2.addVertex(100, 100);
     polygon2.setColor(1.0, 0.5, 1.0, 1.0);
-    polygon2.setPosition(350, 50, 0);
+    polygon2.setPosition(450, 50, 0);
     polygon2.setName("Polygon2");
 
     polygon3.addVertex(0, 0);
-    polygon3.addVertex(200, 0);
-    polygon3.addVertex(0, 200);
-    polygon3.addVertex(200, 200);
+    polygon3.addVertex(100, 0);
+    polygon3.addVertex(0, 100);
+    polygon3.addVertex(100, 100);
     polygon3.setColor(0.5, 1.0, 1.0, 1.0);
-    polygon3.setPosition(650, 50, 0);
+    polygon3.setPosition(800, 50, 0);
     polygon3.setName("Polygon3");
+
+    polygon4.addVertex(0, 0);
+    polygon4.addVertex(100, 0);
+    polygon4.addVertex(0, 100);
+    polygon4.addVertex(100, 100);
+    polygon4.setColor(0.5, 1.0, 0.3, 1.0);
+    polygon4.setPosition(450, 300, 0);
+    polygon4.setName("Polygon4");
+
+    polygon2.moveToFirst();
 
     Quaternion startRot;
     Quaternion endRot;
@@ -58,7 +77,7 @@ void init(){
     endRot.fromAngle(Angle::degToDefault(180));
     rotationaction.setAction(startRot, endRot, 5, false);
     rotationaction.setTarget(polygon3.getEntity());
-    rotationaction.getComponent<ActionComponent>().onStart = onActionStart;
+    timedaction.getComponent<ActionComponent>().onStart = onActionStart;
 
     scaleaction.setAction(Vector3(1,1,1), Vector3(2,2,2), 10, false);
     scaleaction.setTarget(polygon2.getEntity());
@@ -66,13 +85,16 @@ void init(){
     coloraction.setAction(polygon.getColor(), Vector4(0.0, 0.0, 1.0, 1.0), 5, false);
     coloraction.setTarget(polygon.getEntity());
 
-    alphaaction.setAction(1.0, 0.3, 5, false);
+    alphaaction.setAction(1.0, 0.2, 5, false);
     alphaaction.setTarget(polygon2.getEntity());
 
     positionaction.setAction(Vector3(20,20,0), Vector3(200,200,0), 5, true);
     positionaction.setTarget(polygon.getEntity());
     positionaction.setFunctionType(EaseType::ELASTIC_IN_OUT);
     positionaction.setSpeed(1);
+
+    timedaction.setDuration(10);
+    timedaction.getComponent<ActionComponent>().onStep = onActionStep;
 
     Engine::setScene(&scene);
     Engine::setCallTouchInMouseEvent(true);
@@ -86,8 +108,14 @@ void onTouchStart(int pointer, float x, float y) {
     scaleaction.start();
     coloraction.start();
     alphaaction.start();
+    timedaction.start();
+}
+
+void onActionStep(){
+    float angle = M_PI * 2.0 * timedaction.getValue();
+    polygon4.setPosition(cos(angle)*100 + 450, sin(angle)*100 + 300, 0);
 }
 
 void onActionStart(){
-    Log::verbose("Action Start");
+    Log::verbose("Rotation action Start");
 }
